b03b7d60d1763fc5a794d73c71c19d3339fb2f4f
atajan <atadzantajyrov020@gmail.com>
2020-07-03 14:35:09 -0700

    h2

--- web-parts/contact-us.php
+++ web-parts/contact-us.php
@@ -1,4 +1,11 @@
 <section class="tf-contact-us">
 <div class="tf-contact-us__bg">
+    <div class="tf-container">
+        <div class="tf-contact-us__row">
+            <div class="tf-contact-us__col">
+                <h2>siziň üstünligiňiz<span>biziň şatlygymyz</span></h2>
+            </div>
+        </div>
+    </div>
 </div>
 </section>
