bcf72a2cd4256d17633c802589403ed4f6333a80
atajan <atadzantajyrov020@gmail.com>
2020-07-04 14:05:28 -0700

    form

--- web-parts/contact-us.php
+++ web-parts/contact-us.php
@@ -5,6 +5,21 @@
             <div class="tf-contact-us__col">
                 <h2>siziň üstünligiňiz<span>biziň şatlygymyz</span></h2>
             </div>
+            <div class="tf-contact-us__col">
+                <div class="tf-contact-us__form">
+                    <h3>Biz bilen habarlaşyŇ</h3>
+                    <form class="tf-form" id="contactUs" action="">
+                        <input class="tf-form__input" id="firstName" name="first-name" type="text" placeholder="Doly Adyňyz">
+                        <input class="tf-form__input" id="telephone" name="telephone-number" type="number" placeholder="Telefon Belgiňiz">
+                        <select class="tf-form__select" name="services" id="services">
+                        <option value="0">Gyzyklandyrýan hyzmat</option>
+                        <option value="website">Web site</option>
+                        <option value="dizayn">Dizayn</option>
+                        </select>
+                        <input type="submit" class="tf-form__submit" id="formSubmit" value="maňa jaň ediň">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
