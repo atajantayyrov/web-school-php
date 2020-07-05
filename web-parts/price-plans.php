85dff2ce95ce56583cdaa100ab63a9ed77674203
atajan <atadzantajyrov020@gmail.com>
2020-07-05 15:06:14 -0700

    price-plans

--- web-parts/price-plans.php
+++ web-parts/price-plans.php
@@ -22,6 +22,57 @@
                 </ul>
                 <a href="#!" class="tf-price-plan__button">gyzykly</a>
             </div>
+            <div class="tf-price-plan">
+                <div class="tf-price-plan__price">
+                    <span class="tf-price-plan__amount">4000</span>
+                    <span class="tf-price-plan__currency">TMT</span>
+                </div>
+                <div class="tf-price-plan__title">
+                    <h3>Taýyn şablon "Lending"</h3>
+                </div>
+                <ul class="tf-price-plan__list">
+                    <li>Etiam feugiat</li>
+                    <li>Non placerat felis sagittis</li>
+                    <li>Praesent tristique</li>
+                    <li>Sagittis urna et</li>
+                    <li>Vestibulum pellentesque</li>
+                </ul>
+                <a href="#!" class="tf-price-plan__button">gyzykly</a>
+            </div>
+            <div class="tf-price-plan">
+                <div class="tf-price-plan__price">
+                    <span class="tf-price-plan__amount">4000</span>
+                    <span class="tf-price-plan__currency">TMT</span>
+                </div>
+                <div class="tf-price-plan__title">
+                    <h3>Taýyn şablon "Lending"</h3>
+                </div>
+                <ul class="tf-price-plan__list">
+                    <li>Etiam feugiat</li>
+                    <li>Non placerat felis sagittis</li>
+                    <li>Praesent tristique</li>
+                    <li>Sagittis urna et</li>
+                    <li>Vestibulum pellentesque</li>
+                </ul>
+                <a href="#!" class="tf-price-plan__button">gyzykly</a>
+            </div>
+            <div class="tf-price-plan">
+                <div class="tf-price-plan__price">
+                    <span class="tf-price-plan__amount">4000</span>
+                    <span class="tf-price-plan__currency">TMT</span>
+                </div>
+                <div class="tf-price-plan__title">
+                    <h3>Taýyn şablon "Lending"</h3>
+                </div>
+                <ul class="tf-price-plan__list">
+                    <li>Etiam feugiat</li>
+                    <li>Non placerat felis sagittis</li>
+                    <li>Praesent tristique</li>
+                    <li>Sagittis urna et</li>
+                    <li>Vestibulum pellentesque</li>
+                </ul>
+                <a href="#!" class="tf-price-plan__button">gyzykly</a>
+            </div>
         </div>
     </div>
 </section>
