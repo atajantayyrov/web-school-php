1cbac408375bcce93cbe4e76765263f677b6468b
atajan <atadzantajyrov020@gmail.com>
2020-07-06 14:36:51 -0700

    table

--- web-parts/option.php
+++ web-parts/option.php
@@ -8,34 +8,38 @@
         <table>
             <tr>
                 <th></th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Lastname</th>
+                <th>TF</th>
+                <th>Başga Kompaniýa</th>
+                <th>Başga Kompaniýa</th>
             </tr>
             <tr>
-                <td>Peter</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
+                <td>Websaýt görnüşi</td>
+                <td>Griffin<br><span>Täze dizaýn, arassa kod</span></td>
+                <td>Griffin<br><span>Mugt ýüklenen taýyn şablon</span</td>
+                <td>Griffin<br><span>Hili pes mugt şablon</span</td>
             </tr>
             <tr>
-                <td>Peter</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
+                <td>Harjanan Wagt</td>
+                <td>~ 2 hepde<br><span>Dizaýn, kod</span</td>
+                <td>~ min. 4 hepde<br><span>Başga proýektlerden wagt bolanok</span</td>
+                <td>~ min. 6 hepde<br><span>Näbelli sebäpler wagty uzadýar</span</td>
             </tr>
             <tr>
-                <td>Peter</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
+                <td>Hyzmat Hili</td>
+                <td>Ýokary<br><span>Hemme etapdan müşderi habarly</span</td>
+                <td>Pes<br><span>Müşderä diňe netije görkezilýär</span</td>
+                <td>Pes<br><span>Haçan nähili netije boljagy näbelli</span</td>
             </tr>
             <tr>
-                <td>Peter</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
-                <td>Griffin</td>
+                <td>Garantiýa</td>
+                <td>1 aý<br><span>Çykan näsazlyklar mugt</span</td>
+                <td>1 hepde<br><span>Çykan näsazlyklar mugt</span</td>
+                <td>0<br><span>Çykan näsazlyklar tölegli</span</td>
             </tr>
+            </table>
+        </div>
+        <div class="tf-option__text">
+            <p>Etiam feugiat pellentesque odio, non placerat felis sagittis vitae. Praesent tristique sagittis urna et<br> vestibulum. Suspendisse nulla libero, posuere non tristique quis, pretium sed justo.</p>
         </div>
     </div>
 </section>
